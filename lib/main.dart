d53d289bea53daeb07c80df5db3fbfbec2a0c2fa
vyk1262 <vyk1262@gmail.com>
2024-04-23 12:54:14 +0530

    added build for web

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/screens/home.dart';
 
-void main (){
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
